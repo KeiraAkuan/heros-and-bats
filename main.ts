4b03903186e6dee73ee0276ad8d73231072a143e
KeiraAkuan <77209368+KeiraAkuan@users.noreply.github.com>
2021-07-24 14:02:58 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let player2 = sprites.create(img`
+    ........................
+    ........................
+    ...........ccc..........
+    ...........cccc.........
+    .......ccc..ccccccc.....
+    .......cccccc555555cc...
+    ........ccb5555555555c..
+    .....cc..b555555555555c.
+    .....cccb555555ff155555c
+    ......cb55555555ff55d55c
+    ......b5555555555555555c
+    ...cc.b555dd5555bb13bbc.
+    ...cccd55ddddd555b3335c.
+    .....bdddddddddd55b335c.
+    ..cccdddddb55bbddd5555c.
+    ..cccdddddb555bbbbcccc..
+    ...ccddddddb5555cbcdc...
+    ccccbdddddd5cb55cbcc....
+    cddddddddd5555ccbbc.....
+    .cddddddbdd555bbbcc.....
+    ..ccdddbbbdd55cbcdc.....
+    ....ccbbcbddddccdddcc...
+    ......cccdd555dcccccc...
+    ........cccccccc........
+    `, SpriteKind.Player)
+controller.moveSprite(player2)
+game.onUpdateInterval(2000, function() {
+
+let enemy = sprites.create(img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c . . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f b 3 b c 3 b c f b b c c c . 
+    . c b b b b b b c f b c b c c . 
+    . c b b b b b b c b b c b b c . 
+    c b 1 b b b 1 b b b c c c b c . 
+    c b b b b b b b b c c c c c . . 
+    f b c b b b c b b b b f c . . . 
+    f b 1 f f f 1 b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    `, SpriteKind.Enemy)
+    
+    enemy.setFlag(SpriteFlag.AutoDestroy, true)
+    enemy.setPosition(160,  randint(0,120))
+    enemy.setVelocity(-50, 0)
+
+info.setLife(3)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(sprite: Sprite, Enemy: Sprite) {
+    enemy.destroy()
+ info.changeLifeBy(-1)   
+})
+})
